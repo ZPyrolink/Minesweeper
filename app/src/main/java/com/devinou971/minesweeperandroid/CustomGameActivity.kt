8e1261587859c869d9d062cc98424ecdcb0f5f1d
Corenthin Z <38cz74@gmail.com>
2023-03-18 22:33:47 +0100

    Refactor CustomGameActivity

--- app/src/main/java/com/devinou971/minesweeperandroid/CustomGameActivity.kt
+++ app/src/main/java/com/devinou971/minesweeperandroid/CustomGameActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import com.devinou971.minesweeperandroid.utils.Difficulty
 import com.devinou971.minesweeperandroid.utils.ExtraUtils
 import com.devinou971.minesweeperandroid.utils.getIntExtra
 import com.devinou971.minesweeperandroid.utils.putExtras
@@ -18,15 +19,16 @@ class CustomGameActivity : AppCompatActivity() {
         val colsSlider: Slider = findViewById(R.id.colsSlider)
         val rowsSlider: Slider = findViewById(R.id.rowsSlider)
 
-        colsSlider.valueTo = intent.getIntExtra(ExtraUtils.NB_COLS, 10).toFloat()
-        rowsSlider.valueTo = intent.getIntExtra(ExtraUtils.NB_ROWS, 10).toFloat()
-        colsSlider.value = colsSlider.valueTo
-        rowsSlider.value = rowsSlider.valueTo
-
-        val gameButton = findViewById<Button>(R.id.startGameButton)
-        gameButton.setOnClickListener {
-            startGame()
+        intent.getIntExtra(ExtraUtils.NB_COLS, 10).toFloat().also {
+            colsSlider.valueTo = it
+            colsSlider.value = it
         }
+        intent.getIntExtra(ExtraUtils.NB_ROWS, 10).toFloat().also {
+            rowsSlider.valueTo = it
+            rowsSlider.value = it
+        }
+
+        findViewById<Button>(R.id.startGameButton).setOnClickListener { startGame() }
     }
 
     private fun startGame() {
@@ -36,7 +38,7 @@ class CustomGameActivity : AppCompatActivity() {
 
         val nbCols = colsSlider.value.toInt()
         val nbRows = rowsSlider.value.toInt()
-        val nbBombs = (nbRows * nbCols * (bombsSlider.value / 100)).toInt()
+        val nbBombs = Difficulty.nbBombs(nbRows, nbCols, (bombsSlider.value / 100))
 
         val availableHeight = (window.decorView.height * 0.80).toInt()
         val availableWidth = window.decorView.width
@@ -45,9 +47,8 @@ class CustomGameActivity : AppCompatActivity() {
             availableWidth / nbCols else
             availableHeight / nbRows
 
-        val intent = Intent(this, GameActivity::class.java).apply {
+        startActivity(Intent(this, GameActivity::class.java).apply {
             putExtras(nbBombs, nbCols, nbRows, cellSize)
-        }
-        startActivity(intent)
+        })
     }
 }
--- app/src/main/java/com/devinou971/minesweeperandroid/utils/Difficulty.kt
+++ app/src/main/java/com/devinou971/minesweeperandroid/utils/Difficulty.kt
@@ -1,10 +1,15 @@
 package com.devinou971.minesweeperandroid.utils
 
-enum class Difficulty(val id: Int, private val bombFactor: Double) {
+enum class Difficulty(val id: Int, private val bombPercentage: Double) {
     CUSTOM(-1, .0),
     EASY(0, .15),
     NORMAL(1, .22),
     HARD(2, .38);
 
-    fun nbBombs(rows: Int, columns: Int) = (rows * columns * bombFactor).toInt()
+    fun nbBombs(rows: Int, columns: Int) = (rows * columns * bombPercentage).toInt()
+
+    companion object {
+        fun nbBombs(rows: Int, columns: Int, percentage: Float) =
+            (rows * columns * percentage).toInt()
+    }
 }
