901bf5212211a02299b0f23bd7c34f6c58b2702e
Corenthin Z <38cz74@gmail.com>
2023-03-19 21:13:51 +0100

    Refactor Slot

--- app/src/main/java/com/devinou971/minesweeperandroid/classes/Slot.kt
+++ app/src/main/java/com/devinou971/minesweeperandroid/classes/Slot.kt
@@ -4,44 +4,34 @@ import android.graphics.Point
 import com.devinou971.minesweeperandroid.extensions.nextTo
 
 class Slot {
-    var isRevealed : Boolean = false
-    private set
-    var isBomb : Boolean = false
-    var nbBombs : Int = 0
+    var isRevealed: Boolean = false
+        private set
+    var isBomb: Boolean = false
+    var nbBombs: Int = 0
     var isFlagged = false
-    var position = Point(0,0)
+    var position = Point()
 
     fun reveal() {
-        this.isRevealed = true
+        isRevealed = true
     }
 
-    override fun toString(): String {
-        var result = "#"
-        if(isFlagged)
-            result = "F"
-        if(isRevealed)
-            result = if(!isBomb) "$nbBombs" else "B"
-        return result
+    override fun toString() = when {
+        isFlagged -> "F"
+        isRevealed -> if (!isBomb) "$nbBombs" else "B"
+        else -> "#"
     }
 
-    fun nextTo(o:Slot) : Boolean{
-        return this.position.nextTo(o.position)
-    }
+    fun nextTo(o: Slot) = position.nextTo(o.position)
 
-    fun getUnseenNeighbors(grid : MutableList<MutableList<Slot>>) : MutableList<Slot>{
+    fun getUnseenNeighbors(grid: MutableList<MutableList<Slot>>): MutableList<Slot> {
         val neighbors = mutableListOf<Slot>()
-        for(line in grid){
-            neighbors.addAll(line.filter { x->this.nextTo(x)&&!x.isRevealed })
-        }
-
+        grid.forEach { line -> neighbors.addAll(line.filter { x -> nextTo(x) && !x.isRevealed }) }
         return neighbors
     }
 
-    fun xRayView() : String{
-        return if(isBomb) "B" else "$nbBombs"
-    }
+    fun xRayView() = if (isBomb) "B" else "$nbBombs"
 
-    fun hide(){
+    fun hide() {
         isRevealed = false
     }
 }
