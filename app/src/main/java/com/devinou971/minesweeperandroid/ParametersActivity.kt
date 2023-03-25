293dc58271830b62acfbdad74df81c6d7a63b93d
Corenthin Z <38cz74@gmail.com>
2023-03-25 15:27:23 +0100

    Correct warnings

--- app/src/main/java/com/devinou971/minesweeperandroid/ParametersActivity.kt
+++ app/src/main/java/com/devinou971/minesweeperandroid/ParametersActivity.kt
@@ -1,7 +1,6 @@
 package com.devinou971.minesweeperandroid
 
 import android.app.AlertDialog
-import android.content.Context
 import android.graphics.Color
 import android.os.Bundle
 import android.text.InputFilter
