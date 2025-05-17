93346224f157edeb70c092b342d9bd4f833ad547
Corenthin ZOZOR <corenthin.zozor@gmail.com>
2025-05-17 13:40:43 +0200

    Replace gradle into gradle kts

---
+++ build.gradle.kts
@@ -0,0 +1,18 @@
+buildscript {
+    dependencies {
+    }
+}
+
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+plugins {
+    id("com.android.application").version("8.10.0").apply(false)
+    id("com.android.library").version("8.10.0").apply(false)
+    id("org.jetbrains.kotlin.android").version("2.1.21").apply(false)
+    id("org.jetbrains.kotlin.jvm").version("2.1.21").apply(false)
+    id("org.jetbrains.kotlin.plugin.compose").version("2.1.21").apply(false)
+}
+
+
+tasks.register("clean", Delete::class) {
+    delete(rootProject.buildDir)
+}
