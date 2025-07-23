975816b84d2cf37ddbe2b09360beba3872df1b4f
ZPyrolink <38cz74@gmail.com>
2025-07-23 23:02:29 +0200

    Hide system bars

--- app/src/main/java/com/devinou971/minesweeperandroid/MainActivity.kt
+++ app/src/main/java/com/devinou971/minesweeperandroid/MainActivity.kt
@@ -6,6 +6,8 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.WindowInsetsControllerCompat
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.rememberNavController
 import com.devinou971.minesweeperandroid.navigation.Screen
@@ -14,6 +16,8 @@ import com.devinou971.minesweeperandroid.ui.theme.MinesweeperAndroidTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        hideSystemBars()
+
         setContent {
             MinesweeperAndroidTheme {
                 Surface(modifier = Modifier.fillMaxSize()) {
@@ -25,6 +29,13 @@ class MainActivity : ComponentActivity() {
                     )
                 }
             }
+        }
+    }
+
+    private fun hideSystemBars() {
+        WindowInsetsControllerCompat(window, window.decorView).apply {
+            hide(WindowInsetsCompat.Type.systemBars())
+            systemBarsBehavior = WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
         }
     }
 }
